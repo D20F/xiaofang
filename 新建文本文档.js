bb2e5cd85d95355e9d5e3d884a6fab9b2078d84f
小方 <45145249+D20F@users.noreply.github.com>
2019-11-12 15:27:18 +0800

    Add files via upload

---
+++ 新建文本文档.js
@@ -0,0 +1,17 @@
+var array=[1,3,3,3,22,3,1,2,3,4,5,6,7,8,1,2,3,4,5,7,66,6,6,6,4,4]
+    console.log(array)
+    for(var i=0;i<array.length;i++){
+      var ccc=1;
+      for(var l=i+1;l<array.length;l++){
+        if(array[i]==array[l]){
+          array.splice(l,1)
+          l--
+          ccc++
+        } 
+      }
+      var b=array[i]
+      array[i]={s:ccc}
+      array[i][b]=ccc
+      // delete array[i].s;
+    }
+    console.log(array)
