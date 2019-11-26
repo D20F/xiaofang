e6e7ebeda3f441221fe517e6d2512a8326929d47
小方 <45145249+D20F@users.noreply.github.com>
2019-11-26 18:19:12 +0800

    Update js面试.js

--- js面试.js
+++ js面试.js
@@ -8,6 +8,25 @@
 //       |        <--------------          ↗
 //       |           constructor         ⁄
 //      new                         _proto_
-//       |        ⁄                  ⁄
-//       ↓       ⁄                 ⁄
+//       |                           ⁄
+//       ↓                         ⁄
 //   实例对象p  ----------------- ⁄
+
+
+
+// JSON.parse() 方法用于将一个 JSON 字符串转换为对象。
+// JSON.stringify() 方法将一个 JavaScript 值（对象或者数组）转换为一个 JSON 字符串
+// 深拷贝方法一    var obj2=JSON.parse(JSON.stringify(obj1)) 
+// 深拷贝方法二    var obj2=[...obj1]
+// 深拷贝方法三
+function copy(obj) {
+    if(typeof obj == "object") { //判断是否复杂类型
+       var result = obj.constructor == Array ? [] : {};//判断数组类型或是object，数组即result=[]，object即result={}
+        for(let i in obj) {
+            result[i] = typeof obj[i] == "object" ? copy(obj[i]) : obj[i]//判断数据每一项是否是object
+        }
+    } else {
+        var result = obj //基本类型直接拷贝
+    }
+  return result;
+}
