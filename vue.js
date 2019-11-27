74c5e3e4b712457b028fac358d86f3607c6913f4
小方 <45145249+D20F@users.noreply.github.com>
2019-11-27 17:22:40 +0800

    Update vue.js

--- vue.js
+++ vue.js
@@ -2,3 +2,5 @@
 // $emit('enlarge-text', 0.1)" 子组件绑定的函数调用父组件的函数
 // this.$parent.fatherMethod();调用父级的函数
 // this.$refs.mychild.parentHandleclick("嘿嘿嘿");注册ref然后通过ref调用他的函数
+
+// <style scoped>  锁定css
