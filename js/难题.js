b514a7b6c9132159e2d72a546f4306aec4757d18
D20F <2442563148@qq.com>
2019-11-29 16:52:59 +0800

    sad

---
+++ js/难题.js
@@ -0,0 +1,14 @@
+function fun(n,o) {
+    console.log('n',n)
+    console.log('o',o)
+    return {
+      fun:function(m){
+        console.log('m',m)
+        console.log('n',n)
+        return fun(m,n);
+      }
+    };
+  }
+  var a = fun(0);  a.fun(1);  a.fun(2);  a.fun(3);
+  var b = fun(0).fun(1).fun(2).fun(3);
+  var c = fun(0).fun(1);  c.fun(2);  c.fun(3);
