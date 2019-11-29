802b4e82fc62120c78bde774503991dd2de72baf
小方 <45145249+D20F@users.noreply.github.com>
2019-11-29 10:23:59 +0800

    Update 用到的插件.js

--- 用到的插件.js
+++ 用到的插件.js
@@ -1,3 +1,9 @@
 // https://segmentfault.com/a/1190000013508719  
-import { format, parse } from 'date-fns'
-import {Decimal} from 'decimal.js'
+// 日期格式化
+// import { format, parse } from 'date-fns'
+
+// js数字精确
+// import {Decimal} from 'decimal.js'
+
+// three.js  文档
+// https://threejs.org/docs/index.html#manual/zh/introduction/Creating-a-scene
