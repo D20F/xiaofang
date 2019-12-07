e58b1d2ba791d1fa32f21d913cb43424dc0e6473
D20F <2442563148@qq.com>
2019-12-07 15:22:55 +0800

    提交

--- 插件网站/用到的插件.js
+++ 插件网站/用到的插件.js
@@ -8,8 +8,9 @@
 // three.js  文档
 // https://threejs.org/docs/index.html#manual/zh/introduction/Creating-a-scene
 
-
-
-http://www.zzfriend.com/h5game/
-http://www.zzfriend.com/xiazai/youxi/
-http://www.52h5.com/forum.php
+//    https://www.xmind.cn/                                     构建脑图
+//    https://www.processon.com/                                构建脑图和流程图
+//    https://www.uupoop.com/                                   在线ps
+//    https://tinypng.com/                                      压缩图片
+//    https://cli.im/                                           二维码生成
+//    https://www.aconvert.com/cn/video/mp4-to-gif/             转GIF
