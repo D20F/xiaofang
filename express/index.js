208336c2e0533bc87203b29f2e969ca362073010
fb <2442563148@qq.com>
2020-05-13 15:01:53 +0800

    express推送一下

---
+++ express/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 8085;
+const hostname = 'localhost';
+
+app.use(express.static(path.resolve(__dirname, './dist')))
+ 
+app.get('/', (req, res) => {
+	const html = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf-8');
+	res.send(html)
+})
+
+
+app.listen(port, () => { 
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
