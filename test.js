d1c6b82044313f82d2261c8cb13cb929af5c5cb1
fb <2442563148@qq.com>
2020-11-16 16:51:42 +0800

    dsa

--- test.js
+++ test.js
@@ -7,22 +7,7 @@ let arr = [2, 7, 11, 15, 7, 2, 11, 15];
 let arr = [3, 2, 1, 1,2,2, 4, 2,532,421,124,1,2,421,4,412,4124,5,3];
 
 let arr = [3,1,3];
-function quickSort(array) {
-    // 数组必须长于1
-    if (array.length < 1) {
-        return array;
-    }
-    // 拿到数组最后一位
-    let pivot = array[array.length - 1];
-    // 小于等于数组最后一个的全部去左边 
-    let left = array.filter((v, i) => v <= pivot && i != array.length - 1)
-    // 大于等于数组最后一个的全部去右边 
-    let right = array.filter(v => v > pivot)
-    console.log("left",left)
-    console.log("right",right)
-    console.log("pivot",pivot)
-    return [...quickSort(left), pivot, ...quickSort(right)]
-}
+
 quickSort(arr)
 
 
